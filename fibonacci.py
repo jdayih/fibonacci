9990c72b02a724d7ec2ffafa0d85f99e42d905bc
Jasmine <jasminedhaliwal29@gmail.com>
2020-02-15 23:52:21 +0000

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,5 @@
-Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def fibonacci(num):
+#A function that produces 'num' number of fibonacci numbers
+def fibonacci(num):
 	x = [1]
 	for i in range(1,num):
 		if i==1:
